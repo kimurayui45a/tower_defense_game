774b68ecbf6ceede32073e6decdf94e0da843e6a
yuikimura <edikanorusiruru@outlook.jp>
2025-08-10 19:07:13 +0900

    プッシュ4

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -19,16 +19,50 @@ public class GameManager : MonoBehaviour
 
     public int maxEnemyCount;
 
+    /// <summary>
+    /// ゲームの状態
+    /// </summary>
+    public enum GameState
+    {
+        Preparate,       // ゲーム開始前の準備中
+        Play,            // ゲームプレイ中
+        Stop,            // ゲーム内の処理の一時停止中
+        GameUp           // ゲーム終了(ゲームオーバー、クリア両方)
+    }
+
+    // 現在の GameState の状態。
+    // 上記の GameState の列挙子が１つだけ代入されるので、他の GameState と競合しない
+    public GameState currentGameState;
 
     void Start()
     {
+
+        // ゲームの進行状態を準備中に設定
+        SetGameState(GameState.Preparate);
+
+
+        // TODO ゲームデータを初期化
+
+
+        // TODO ステージの設定 + ステージごとの PathData を設定
+
         // キャラ配置用ポップアップの生成と設定
         StartCoroutine(charaGenerator.SetUpCharaGenerator(this));
 
+        // TODO 拠点の設定
+
+
+        // TODO オープニング演出再生
+
         isEnemyGenerate = true;
+
+        // ゲームの進行状態をプレイ中に変更
+        SetGameState(GameState.Play);
 
         // 敵の生成準備開始
         StartCoroutine(enemyGenerator.PreparateEnemyGenerate(this));
+
+        // TODO カレンシーの自動獲得処理の開始
     }
 
     /// <summary>
@@ -54,4 +88,14 @@ public class GameManager : MonoBehaviour
             isEnemyGenerate = false;
         }
     }
+
+    /// <summary>
+    /// GameState の変更
+    /// </summary>
+    /// <param name="nextGameState"></param>
+    public void SetGameState(GameState nextGameState)
+    {
+        currentGameState = nextGameState;
+    }
+
 }
